47cc2e0a2714fa6fa0a5cf6a7d22f17643e2dba4
Md Habibur Rahman <habib.cse.ru@gmail.com>
2023-08-12 19:48:46 +0600

    first problem solved

---
+++ exampleOne.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exampleOne{
+public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in);
+
+    int number = scanner.nextInt();
+
+    if(number == 0){
+        System.out.println("Zero");
+    }
+    else if(number <0){
+        System.out.println("Negative");
+    }
+    else{
+        System.out.println("Positive");
+    }
+}
+}
